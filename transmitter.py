f96cbcb2673d7e66590127cab805895d27d156fa
Naz <kazinazmulhasan@gmail.com>
2016-11-17 09:07:20 -0700

    fixing data parsing

--- transmitter.py
+++ transmitter.py
@@ -98,7 +98,8 @@ class Transmitter(Thread):
 		for target in self.config.targets:
 			for i in range(len(self.config.targets[target])):
 				self.config.targets[target][i] = self.config.dependents[self.config.targets[target][i]]
-			self.config.targets[target] = sum(self.config.targets[target])
+			# self.config.targets[target] = sum(self.config.targets[target])
+		print(self.config.dependents)
 		print(self.config.targets)
 		
 		self.logger.log("data collected")
